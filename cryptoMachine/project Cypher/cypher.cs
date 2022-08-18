937a25dfc59bf036dbc3d4d4eb98aeea424743b5
Abbos <atamura.shin@gmail.com>
2022-08-18 12:40:18 +0300

    humble start

---
+++ cryptoMachine/project Cypher/cypher.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string alphabetENG = "abcdefghijklmnopqrstuvwxyz";
+string alphabetRUS = "абвгдеёзжийклмнопрстуфчцчшщъыьэюя";
+Console.WriteLine();
